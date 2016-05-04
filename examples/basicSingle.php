f9cf44c5d79416f6eaadbe62c90f527fc7c0d0a4
Ing. Petr Suchy <suchy@wamos.cz>
2016-05-04 13:26:57 +0200

    *edit - format of examplex

--- examples/basicSingle.php
+++ examples/basicSingle.php
@@ -7,15 +7,14 @@ require __DIR__ . '/../src/RollingCurl/Request.php';
 
 $rollingCurl = new \RollingCurl\RollingCurl();
 $rollingCurl
-    ->get('http://google.com')
-    ->setCallback(function(\RollingCurl\Request $request, \RollingCurl\RollingCurl $rollingCurl) {
-        if (preg_match("#<title>(.*)</title>#i", $request->getResponseText(), $out)) {
-            $title = $out[1];
-        }
-        else {
-            $title = '[No Title Tag Found]';
-        }
-        echo "Fetch complete for (" . $request->getUrl() . ") $title " . PHP_EOL;
-    })
-    ->execute();
+		->get('http://google.com')
+		->setCallback(function(\RollingCurl\Request $request, \RollingCurl\RollingCurl $rollingCurl) {
+			if (preg_match("#<title>(.*)</title>#i", $request->getResponseText(), $out)) {
+				$title = $out[1];
+			} else {
+				$title = '[No Title Tag Found]';
+			}
+			echo "Fetch complete for (" . $request->getUrl() . ") $title " . PHP_EOL;
+		})
+		->execute();
 ;
--- examples/searchScrapePostProcess.php
+++ examples/searchScrapePostProcess.php
@@ -15,8 +15,8 @@ require __DIR__ . '/../src/RollingCurl/Request.php';
 $rollingCurl = new \RollingCurl\RollingCurl();
 
 for ($i = 0; $i <= 500; $i+=10) {
-    // https://www.google.com/search?q=curl&start=10
-    $rollingCurl->get('https://www.google.com/search?q=curl&start=' . $i);
+	// https://www.google.com/search?q=curl&start=10
+	$rollingCurl->get('https://www.google.com/search?q=curl&start=' . $i);
 }
 
 $results = array();
@@ -24,19 +24,19 @@ $results = array();
 $start = microtime(true);
 echo "Fetching..." . PHP_EOL;
 $rollingCurl
-    ->setSimultaneousLimit(10)
-    ->execute();
+		->setSimultaneousLimit(10)
+		->execute();
 ;
 echo "...done in " . (microtime(true) - $start) . PHP_EOL;
 
 foreach ($rollingCurl->getCompletedRequests() as $request) {
-    if (preg_match_all('#<h3 class="r"><a href="([^"]+)">(.*)</a></h3>#iU', $request->getResponseText(), $out)) {
-        foreach ($out[1] as $idx => $url) {
-            parse_str(parse_url($url, PHP_URL_QUERY), $params);
-            $results[$params['q']] = strip_tags($out[2][$idx]);
-        }
-    }
-    echo "Processsed (" . $request->getUrl() . ")" . PHP_EOL;
+	if (preg_match_all('#<h3 class="r"><a href="([^"]+)">(.*)</a></h3>#iU', $request->getResponseText(), $out)) {
+		foreach ($out[1] as $idx => $url) {
+			parse_str(parse_url($url, PHP_URL_QUERY), $params);
+			$results[$params['q']] = strip_tags($out[2][$idx]);
+		}
+	}
+	echo "Processsed (" . $request->getUrl() . ")" . PHP_EOL;
 }
 
 echo "All results: " . PHP_EOL;
